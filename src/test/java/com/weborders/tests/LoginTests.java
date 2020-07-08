39148a95e33717547d4be8b320083565fffd6fd5
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-07-08 16:54:54 -0400

    update

--- src/test/java/com/weborders/tests/LoginTests.java
+++ src/test/java/com/weborders/tests/LoginTests.java
@@ -1,6 +1,16 @@
 package com.weborders.tests;
 
-public class LoginTests {
+import com.weborders.pages.LoginPage;
+import com.weborders.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class LoginTests extends TestBase {
 
+    @Test(description = "Just login and verify title")
+    public void loginTest() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Web Orders", "Page title is wrong!");
+    }
 }
