aa81e55c9cb54dd4e9eee0ccd89e02fe8a11aee3
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-07-08 16:42:56 -0400

    update

--- src/test/java/com/weborders/pages/LoginPage.java
+++ src/test/java/com/weborders/pages/LoginPage.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.security.Key;
-
 public class LoginPage extends BasePage {
 
     @FindBy(id = "ctl00_MainContent_username")
