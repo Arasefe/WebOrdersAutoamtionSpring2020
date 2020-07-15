b6605303d3c1aeba45ca97bf0275708bc433cd0b
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-07-15 17:03:00 -0400

    Completed OrderPage.java

--- src/test/java/com/weborders/tests/LoginTests.java
+++ src/test/java/com/weborders/tests/LoginTests.java
@@ -1,6 +1,7 @@
 package com.weborders.tests;
 
 import com.weborders.pages.LoginPage;
+import com.weborders.utilities.BrowserUtils;
 import com.weborders.utilities.Driver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -16,5 +17,6 @@ public class LoginTests extends TestBase {
         String actual = Driver.getDriver().getTitle();
         String expected = "Web Orders";
         Assert.assertEquals(actual, expected, "Page title is wrong!");
+        BrowserUtils.takeScreenshot();
     }
 }
